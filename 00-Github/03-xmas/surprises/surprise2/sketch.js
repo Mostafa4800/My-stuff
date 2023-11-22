7406ecf783180e8457eca34e02e0542c48fc425c
Mostafa4800 <mostafaabbashassan@gmail.com>
2023-11-22 13:25:22 +0100

    Vine surprise

--- 00-Github/03-xmas/surprises/surprise2/sketch.js
+++ 00-Github/03-xmas/surprises/surprise2/sketch.js
@@ -1,31 +1,29 @@
-let apiKey = 'AIzaSyAF2z2DthVDBVU9RUueYdQQdCUkmtY-wVs';
 let ellipseStartR = 1920;
 let ellipseEndR = 0;
 let backButton;
 let videoElement;
 let backgroundImg;
 let speed = 20;
-let startVideoFrame;
-let videoId = 'bikjZBdfDZY';
-
-function preload() {
-    loadJSON(
-        `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet,contentDetails`,
-        gotVideoData
-    );
-}
-
-function gotVideoData(data) {
-    let videoUrl = `https://www.youtube.com/embed/${videoId}?controls=0&autoplay=1`;
-    videoElement = createDiv(`<iframe id="youtubeVideo" width="${windowWidth}" height="${windowHeight}" src="${videoUrl}" frameborder="0" allowfullscreen></iframe>`);
-}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    backgroundImg = loadImage("assets/beeMovie.png");
 
+    videoElement = createVideo("assets/vine.mp4");
+    videoElement.size(width, height);
+    videoElement.hide();
+
+    videoElement.onended(() => {
+        backButton.show();
+    });
+
+    backgroundImg = loadImage("assets/vine.png");
+
+    // Create the button but initially hide it
     backButton = createButton("Go back");
     backButton.position(20, 20);
+    backButton.hide();
+
+    // Set up mousePressed event for the button
     backButton.mousePressed(goBack);
 }
 
@@ -36,24 +34,33 @@ function goBack() {
         fill("red");
         ellipse(width / 2, height / 2, ellipseEndR);
         ellipseEndR += speed;
-        if (frameCount > startVideoFrame) {
-            // Video should start playing automatically
-        }
-        requestAnimationFrame(goBack);
+        requestAnimationFrame(goBack); // Call the function again on the next frame
     } else {
         window.location.href = "../../index.html";
     }
 }
 
 function draw() {
+    // Draw the background image
     image(backgroundImg, 0, 0, width, height);
+
+    // Draw the video first
+    image(videoElement, 0, 0, width, height);
+
+    // This code is just to make it more convenient when switched from page to page
+    if (ellipseStartR > 0) {
+        noStroke();
+        fill("red");
+        ellipse(width / 2, height / 2, ellipseStartR);
+        ellipseStartR -= speed;
+        // If the circle is fully drawn, show the button
+        if (ellipseStartR <= 0) {
+            backButton.show();
+            videoElement.play();
+        }
+    }
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-
-    //fuck me i cant make it resize fuck that
-    /* // Update the dimensions of the iframe when the window is resized
-    videoElement.child().attribute('width', windowWidth);
-    videoElement.child().attribute('height', windowHeight);*/
 }
