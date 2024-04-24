475d79863e1b84c184082963d926f4efadd01294
Mostafa4800 <mostafaabbashassan@gmail.com>
2024-04-24 12:54:06 +0200

    Minesweeper

---
+++ 00-Github/10-exams-project/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    var canvas = createCanvas(430, 932);
+    canvas.parent('canvas'); //'canvas' is the id of the target HTML element
+}
+
+function draw() {
+  background(220);
+  // Draw a circle
+ellipse(50, 50, 80, 80);
+}
